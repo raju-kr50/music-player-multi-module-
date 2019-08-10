4fd3a1a1f8c0cc1fde048fd7516496249b9973b1
raju.kumar <raju.kumar@cgi.com>
2019-08-10 15:55:19 +0530

    Added Logger

--- track-service/src/main/java/com/stackroute/trackservice/TrackServiceApplication.java
+++ track-service/src/main/java/com/stackroute/trackservice/TrackServiceApplication.java
@@ -1,5 +1,7 @@
 package com.stackroute.trackservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -7,11 +9,18 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @EnableDiscoveryClient
 public class TrackServiceApplication {
+	private static Logger LOGGER=  LoggerFactory.getLogger(TrackServiceApplication.class);
 
 	//main method
 	public static void main(String[] args)
 	{
+
 		SpringApplication.run(TrackServiceApplication.class, args);
+
+		LOGGER.debug("This is a debug message");
+		LOGGER.info("This is an info message");
+		LOGGER.warn("This is a warn message");
+		LOGGER.error("This is an error message");
 	}
 
 }
--- user-service/src/main/java/com/stackroute/UserServiceApplication.java
+++ user-service/src/main/java/com/stackroute/UserServiceApplication.java
@@ -1,12 +1,21 @@
 package com.stackroute;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.logging.Logger;
 
 @SpringBootApplication
 public class UserServiceApplication {
 
+//	Logger logger = (Logger) LoggerFactory.getLogger(this.getClass());
+
 	public static void main(String[] args) {
+//		LOGGER.debug("This is a debug message");
+//		LOGGER.info("This is an info message");
+//		LOGGER.warn("This is a warn message");
+//		LOGGER.error("This is an error message");
 		SpringApplication.run(UserServiceApplication.class, args);
 	}
 
